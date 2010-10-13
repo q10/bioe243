99be1c039f180266095ae45a22ddc707cd5ee821
q10 <q101100111000@gmail.com>
2010-10-12 23:34:10 -0700

    finishing hw3.3

---
+++ hw3/hw3.3.py
@@ -0,0 +1,95 @@
+from hw3 import *
+from common import *
+from math import exp, pi
+from numpy import histogram
+import pylab
+'''
+## HW3.3a STUFF
+
+r = BoxMueller(50232364960420517)
+x_histogram, y_histogram = [], []
+for i in range(0,10000):
+    (x,y) = r.generate()
+    x_histogram.append(x)
+    y_histogram.append(y)
+
+x_histogram = histogram(x_histogram, bins=70)
+y_histogram = histogram(y_histogram, bins=70)
+
+# save data
+f = open('hw3.3a.x_histogram.dat','w')
+print >>f, x_histogram
+f.close()
+f = open('hw3.3a.y_histogram.dat','w')
+print >>f, y_histogram
+f.close()
+
+# plot data and save
+pylab.clf()
+(n, bins) = x_histogram
+pylab.plot(.5*(bins[1:]+bins[:-1]), n)
+pylab.savefig('hw3.3a.x_histogram.png')
+
+pylab.clf()
+(n, bins) = y_histogram
+pylab.plot(.5*(bins[1:]+bins[:-1]), n)
+pylab.savefig('hw3.3a.y_histogram.png')
+'''
+
+
+## HW3.3c STUFF
+
+steps = 5000
+
+VV_sim = VelocityVerlet("LJ_108_1.txt")
+FE_sim = ForwardEuler("LJ_108_1.txt")
+
+avg_VV_KE, avg_FE_KE = 0.0, 0.0
+time = [0.0]
+VV_KE = [VV_sim.system_kinetic_energy]
+VV_PE = [VV_sim.system_potential_energy]
+VV_TE = [VV_sim.system_total_energy]
+FE_KE = [FE_sim.system_kinetic_energy]
+FE_PE = [FE_sim.system_potential_energy]
+FE_TE = [FE_sim.system_total_energy]
+
+for i in range(steps):
+    print "step " + str(i) + " running..."
+    VV_sim.MD_step()
+    FE_sim.MD_step()
+
+    avg_VV_KE += VV_sim.system_kinetic_energy
+    avg_FE_KE += FE_sim.system_kinetic_energy
+
+    time.append(VV_sim.time)
+    VV_KE.append(VV_sim.system_kinetic_energy)
+    VV_PE.append(VV_sim.system_potential_energy)
+    VV_TE.append(VV_sim.system_total_energy)
+    FE_KE.append(FE_sim.system_kinetic_energy)
+    FE_PE.append(FE_sim.system_potential_energy)
+    FE_TE.append(FE_sim.system_total_energy)
+
+avg_VV_KE /= steps
+avg_FE_KE /= steps
+avg_VV_T = (2*avg_VV_KE)/((3*len(VV_sim.particles))-3)
+avg_FE_T = (2*avg_FE_KE)/((3*len(FE_sim.particles))-3)
+
+
+f = open('hw3.3c.results','w')
+print >>f, "Average temperature in Forward Euler: " + str(avg_VV_T)
+print >>f, "Average temperature in Velocity Verlet: " + str(avg_FE_T) + "\n\n"
+for i in range(steps+1):
+    print >>f, str(time[i]) + "\t" + str(VV_KE[i]) + "\t" + str(VV_PE[i]) + "\t" + str(VV_TE[i]) + "\t" + str(FE_KE[i]) + "\t" + str(FE_PE[i]) + "\t" + str(FE_TE[i]) + "\n"
+f.close()
+
+pylab.clf()
+pylab.plot(time, VV_KE, color='blue')
+pylab.plot(time, VV_PE, color='red')
+pylab.plot(time, VV_TE, color='green')
+pylab.savefig('hw3.3c.VV.png')
+
+pylab.clf()
+pylab.plot(time, FE_KE, color='blue')
+pylab.plot(time, FE_PE, color='red')
+pylab.plot(time, FE_TE, color='green')
+pylab.savefig('hw3.3c.FE.png')
--- hw3/hw3.py
+++ hw3/hw3.py
@@ -4,26 +4,36 @@ from common import Ran3, BoxMueller
 RHO = 0.8442
 MY_SEED = 59375327573
 DT = 0.001
+DTT = DT**2.0
+TARGET_TEMP = 0.742
 # Because RHO* is 0.8442 and we assumed that sigma is 1,
 # then RHO* = (N/V)(sigma^3) = N/V
 # so V = N/RHO* = 127.93177
 # so cube side length is 5.03878858
-CUBE_BOUNDARY = 5.03878858
+CUBE_BOUNDARY = 5.038788574147522
 HALF_BOUNDARY = CUBE_BOUNDARY/2.0
 
 class Particle:
     def __init__(self, x, y, z):
         self.x, self.y, self.z = x, y, z
         self.vx, self.vy, self.vz = 0, 0, 0
-        self.fx, self.fy, self.fz, = 0, 0, 0        
+        self.fx, self.fy, self.fz, = 0, 0, 0
+        # mass, generalized to suit mass matrices
         self.mx, self.my, self.mz = 1, 1, 1
         self.energy = 0
 
     def __str__(self):
-        position = "Particle (" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")\n"
+        position = "Position (" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")\n"
         velocity = "Velocity (" + str(self.vx) + ", " + str(self.vy) + ", " + str(self.vz) + ")\n"
         force = "Force ("+ str(self.fx) + ", " + str(self.fy) + ", " + str(self.fz) + ")\n"
-        return  position + velocity + force + "Energy = " + str(self.energy)
+        return position + velocity + force + "Energy = " + str(self.energy)
+
+    def position(self):
+        return (self.x, self.y, self.z)
+    def velocity(self):
+        return (self.vx, self.vy, self.vz)
+    def force(self):
+        return (self.fx, self.fy, self.fz)
     
     # boundary conditions
     def add_position(self, dx, dy, dz):
@@ -53,41 +63,91 @@ class Particle:
 
 class Algorithm:
     def __init__(self, filename):
-        self.BoxMueller = BoxMueller(MY_SEED)
-        self.system_energy, self.energy_truncation = 0, 0
-        self.particles = []    
+        self.system_potential_energy, self.system_kinetic_energy, self.system_total_energy, self.energy_truncation = 0.0, 0.0, 0.0, 0.0
+        self.time, self.system_average_energy_per_particle = 0.0, 0.0
+        self.particles = []
 
-        # initialize particles and positions
-        file = open(filename, 'r')
-        line = file.readline()        
-        while line != '':
-            a = map(float, line.strip().replace(',', ' ').split())
-            self.particles.append(Particle(a[0], a[1], a[2]))
-            line = file.readline()
+        # initialize particles, positions, and velocities
+        r_file = open(filename, 'r')
+        v_file = open('init_velocities.out', 'r')
+        r_line = r_file.readline()
+        v_line = v_file.readline()
 
-        # initialize forces and energies
+        while r_line != '' and v_line != '':
+            r = map(float, r_line.strip().replace(',', ' ').split())
+            v = map(float, v_line.strip().replace(',', ' ').split())
+            tmp = Particle(r[0], r[1], r[2])
+            tmp.add_velocity(v[0], v[1], v[2])
+            self.particles.append(tmp)
+            r_line = r_file.readline()
+            v_line = v_file.readline()
+        r_file.close()
+        v_file.close()
+
+        # initialize energies
         for first in self.particles:
             for other in self.particles:
                 if other is not first:
-                    self.add_force_between(first, other)
-                    self.update_energy(first, other)
+                    self.add_energy(first, other)
 
+        # initialize forces
+        for tmp_j in range(len(self.particles)-1):
+            for tmp_k in range(tmp_j+1, len(self.particles)):
+                self.add_force_between(self.particles[tmp_j], self.particles[tmp_k])
+        
         # calculate energy truncation
         # the integral evaluates to (8/9)*PI*rho*N*((sigma/rc^9)-3(sigma/rc^3))
         r_cut = 1.0/HALF_BOUNDARY
-        self.energy_truncation = 8.0 * pi * len(self.particles) * RHO * ((r_cut**9)-(3*(r_cut**3))) / 9.0
-        
-    def update_system_energy(self):
-        self.system_energy = 0.0
-        for first in self.particles:
-            self.system_energy += first.energy
-        self.system_energy /= 2.0
+        self.energy_truncation = 8.0 * pi * len(self.particles) * RHO * ((r_cut**9.0)-(3.0*(r_cut**3.0))) / 9.0
+        self.update_observables()
 
-    def update_energy(self, first, other):
+    def update_observables(self):
+        self.update_system_potential_energy()
+        self.update_system_kinetic_energy()
+        self.system_total_energy = self.system_potential_energy + self.system_kinetic_energy + self.energy_truncation
+        self.average_energy_per_particle = self.system_total_energy / len(self.particles)
+
+    def velocity_rescale(self):
+        # T* = Tk_b / e, so T* = sum(mv^2)/3N
+        current_temp = 0.0
+        for first in self.particles:
+            current_temp += (first.mx*(first.vx**2)) + (first.my*(first.vy**2)) + (first.mz*(first.vz**2))
+        current_temp /= 3*len(self.particles)
+        
+        if abs(current_temp-TARGET_TEMP) > 0.001:
+            gamma = sqrt(TARGET_TEMP/current_temp)
+            for first in self.particles:
+                first.vx, first.vy, first.vz = first.vx*gamma, first.vy*gamma, first.vz*gamma
+        
+    def update_system_potential_energy(self):
+        self.system_potential_energy = 0.0
+        for first in self.particles:
+            self.system_potential_energy += first.energy
+        self.system_potential_energy /= 2.0
+
+    def update_system_kinetic_energy(self):
+        self.system_kinetic_energy = 0.0
+        for first in self.particles:
+            self.system_kinetic_energy += (first.mx*(first.vx**2.0)) + (first.my*(first.vy**2.0)) + (first.mz*(first.vz**2.0))
+        self.system_kinetic_energy /= 2.0
+        
+    def add_energy(self, first, other):
         (dx, dy, dz) = self.mirror_convention(first.x-other.x, first.y-other.y, first.z-other.z)
-        r = sqrt((dx**2)+(dy**2)+(dz**2))        
-        # calculate and add LJ potential
+        r = sqrt((dx**2.0)+(dy**2.0)+(dz**2.0))
         first.energy += 4.0 * (((1/r)**12.0) - ((1/r)**6.0))
+        
+    def update_energy_between(self, first, other, old_coords):
+        (x, y, z) = old_coords
+        (odx, ody, odz) = self.mirror_convention(x-other.x, y-other.y, z-other.z)
+        (dx, dy, dz) = self.mirror_convention(first.x-other.x, first.y-other.y, first.z-other.z)
+        
+        o_r, r = sqrt((odx**2.0)+(ody**2.0)+(odz**2.0)), sqrt((dx**2.0)+(dy**2.0)+(dz**2.0))        
+
+        # calculate and add LJ potential, remove old LJ potential in old particle
+        odE = 4.0 * (((1/o_r)**12.0) - ((1/o_r)**6.0))
+        dE = 4.0 * (((1/r)**12.0) - ((1/r)**6.0))
+        first.energy += dE
+        other.energy += -odE + dE
 
     def collect_old_df_values_for(self, first):
         old_df_values = []
@@ -99,23 +159,15 @@ class Algorithm:
     # calculate and add new force between two particles
     def add_force_between(self, first, other):        
         (dfx, dfy, dfz) = self.force_between(first, other)
-
-        # account for double-counting of forces in the for-loop, by dividing force by 2
-        dfx, dfy, dfz = dfx/2.0, dfy/2.0, dfz/2.0
-
-        first.add_force(dfx, dfy, dfz)
-        other.add_force(-dfx, -dfy, -dfz)
-
-    # updates the force between two particles
-    def update_force_between(self, first, other, old_df_values, i):
-        # remove old force
-        (dfx, dfy, dfz) = old_df_values[i]
-
-        # account for double-counting of forces in the for-loop, by dividing force by 2
-        dfx, dfy, dfz = dfx/2.0, dfy/2.0, dfz/2.0
-
         first.add_force(-dfx, -dfy, -dfz)
         other.add_force(dfx, dfy, dfz)
+
+    # updates the force between two particles
+    def update_force_between(self, first, other, old_df_values, k):
+        # remove old force
+        (dfx, dfy, dfz) = old_df_values[k]
+        first.add_force(dfx, dfy, dfz)
+        other.add_force(-dfx, -dfy, -dfz)
 
         # calculate and add new force
         self.add_force_between(first, other)
@@ -123,8 +175,8 @@ class Algorithm:
     # calculate force in between two particles        
     def force_between(self, first, other):
         (dx, dy, dz) = self.mirror_convention(first.x-other.x, first.y-other.y, first.z-other.z)
-        r = sqrt((dx**2)+(dy**2)+(dz**2))
-        tmp_force = 24 * ((2 * ((1/r)**14.0)) - ((1/r)**8.0))
+        r = sqrt((dx**2.0)+(dy**2.0)+(dz**2.0))
+        tmp_force = -24.0 * ((2.0 * ((1/r)**14.0)) - ((1/r)**8.0))
         return (tmp_force*dx, tmp_force*dy, tmp_force*dz)
 
     # Enforce mirror image convention
@@ -145,13 +197,15 @@ class Algorithm:
 
 
 class ForwardEuler(Algorithm):
-    def __init__(self):
+    def __init__(self, filename="LJ_108_1.txt"):
         Algorithm.__init__(self, filename)
 
     def MD_step(self):
+        self.time += DT
         for first in self.particles:
-            # collect old df values
+            # collect old df values and old coords
             old_df_values = self.collect_old_df_values_for(first)
+            old_coords = first.position()
             
             # update position
             first.add_position(first.vx*DT, first.vy*DT, first.vz*DT)
@@ -162,27 +216,28 @@ class ForwardEuler(Algorithm):
             # erase old forces between this particle and the rest
             # replace with new forces
             # update energy of particle using same loop
-            i, first.energy = 0, 0
+            k, first.energy = 0, 0.0
             for other in self.particles:
                 if other is not first:
-                    self.update_force_between(first, other, old_df_values, i)
-                    self.update_energy(first, other)
-                    i+=1
-   
-    def averages(self):
-        pass
+                    self.update_force_between(first, other, old_df_values, k)
+                    self.update_energy_between(first, other, old_coords)
+                    k+=1
+
+            self.velocity_rescale()
+
+            self.update_observables()
 
 
 class VelocityVerlet(Algorithm):
-    def __init__(self, filename):
+    def __init__(self, filename="LJ_108_1.txt"):
         Algorithm.__init__(self, filename)
-        #initialize velocities
         
     def MD_step(self):
-        DTT = DT**2
+        self.time += DT
         for first in self.particles:
-            # collect old df values
+            # collect old df and dE values
             old_df_values = self.collect_old_df_values_for(first)
+            old_coords = first.position()                
             
             # second terms of Taylor expansion, for less computing in next 2 steps
             sx = first.fx/(2*first.mx)
@@ -196,28 +251,27 @@ class VelocityVerlet(Algorithm):
             first.add_position(dx, dy, dz)
 
             # update velocity at half step with current forces
-            dvx = sz*DT
-            dvy = sz*DT
+            dvx = sx*DT
+            dvy = sy*DT
             dvz = sz*DT
             first.add_velocity(dvx, dvy, dvz)
 
             # erase old forces between this particle and the rest
             # use new positions to calculate new forces, and
             # use same loop to update energy of particle
-            i, first.energy = 0, 0
+            k, first.energy = 0, 0.0
             for other in self.particles:
                 if other is not first:
-                    self.update_force_between(first, other, old_df_values, i)
-                    self.update_energy(first, other)
-                    i+=1
+                    self.update_force_between(first, other, old_df_values, k)
+                    self.update_energy_between(first, other, old_coords)
+                    k+=1
 
             # update velocity at full step with new forces
             dvx = (first.fx*DT)/(2*first.mx)
             dvy = (first.fy*DT)/(2*first.my)
             dvz = (first.fz*DT)/(2*first.mz)
             first.add_velocity(dvx, dvy, dvz)
+            
+            self.velocity_rescale()
 
-            # update total system energy
-            self.update_system_energy()
-    def averages(self):
-        pass
+            self.update_observables()
